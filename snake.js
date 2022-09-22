3e7c5993bd779850823ba0ef195a002dc2936a8a
Dmytro Maksiuta <d.maksiuta18@gmail.com>
2022-09-23 01:52:56 +0300

    add gameOver and change minor thigs

--- snake.js
+++ snake.js
@@ -2,11 +2,13 @@ const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 const width = canvas.width
 const height = canvas.height;
-
+let score = 0;
 const blockSize = 10;
-const score = 0;
 const widthInBlocks = width / blockSize;
 const heightInBlocks = height / blockSize;
+
+const result = document.querySelector('.score');
+result.textContent = score;
 
 const drawBorder = function () {
   ctx.fillStyle = 'Grey';
@@ -15,23 +17,13 @@ const drawBorder = function () {
   ctx.fillRect(0, 0, blockSize, height);
   ctx.fillRect(width - blockSize, 0, blockSize, height);
 };
-// drawBorder();
+
+
 
 const Block = function (col, row) {
   this.col = col;
   this.row = row;
 };
-
-const circle = function (x, y, radius, fillCircle) {
-  ctx.beginPath();
-  ctx.arc(x, y, radius, 0, Math.PI * 2, false);
-  if (fillCircle) {
-    ctx.fill();
-  } else {
-    ctx.stroke();
-  }
-};
-
 
 Block.prototype.drawSquare = function (color) {
   const x = this.col * blockSize;
@@ -51,8 +43,32 @@ Block.prototype.equal = function (otherBlock) {
   return this.col === otherBlock.col && this.row === otherBlock.row
 };
 
-// const sampleCircle = new Block(3, 3);
-// sampleCircle.drawCircle('Blue');
+const Apple = function () {
+  this.position = new Block(3, 3);
+};
+
+Apple.prototype.draw = function () {
+  this.position.drawCircle('Green');
+}
+
+Apple.prototype.move = function () {
+  const randomCol = Math.floor(Math.random() * (widthInBlocks - 2)) + 1;
+  const randomRow = Math.floor(Math.random() * (heightInBlocks - 2)) + 1;
+  this.position = new Block(randomCol, randomRow);
+};
+
+const apple = new Apple();
+
+const circle = function (x, y, radius, fillCircle) {
+  ctx.beginPath();
+  ctx.arc(x, y, radius, 0, Math.PI * 2, false);
+  if (fillCircle) {
+    ctx.fill();
+  } else {
+    ctx.stroke();
+  }
+};
+
 
 const Snake = function () {
   this.segments = [
@@ -67,7 +83,12 @@ const Snake = function () {
 
 Snake.prototype.draw = function () {
   for (let i = 0; i < this.segments.length; i++) {
-    this.segments[i].drawSquare('Green');
+    this.segments[0].drawSquare('Red');
+    if (i % 2) {
+      this.segments[i].drawSquare('Yellow');
+    } else {
+      this.segments[i].drawSquare('Blue');
+    }   
   }
 };
 
@@ -114,15 +135,12 @@ Snake.prototype.move = function () {
   this.segments.unshift(newHead);
 
   if (newHead.equal(apple.position)) {
-    score++;
+    result.textContent++;
     apple.move();
   } else {
     this.segments.pop();
   }
 };
-
-const snake = new Snake();
-snake.draw();
 
 Snake.prototype.setDirection = function (newDirection) {
   if (this.direction === 'up' && newDirection === 'down') {
@@ -138,6 +156,8 @@ Snake.prototype.setDirection = function (newDirection) {
   this.nextDirection = newDirection;
 };
 
+let snake = new Snake();
+
 const directions = {
   37: 'left',
   38: 'up',
@@ -152,29 +172,16 @@ $('body').keydown(function (event) {
   }
 });
 
-const Apple = function () {
-  this.position = new Block(3, 3);
-};
-
-Apple.prototype.draw = function () {
-  this.position.drawCircle('Blue');
-}
-
-Apple.prototype.move = function () {
-  const randomCol = Math.floor(Math.random() * (widthInBlocks - 2)) + 1;
-  const randomRow = Math.floor(Math.random() * (heightInBlocks - 2)) + 1;
-  this.position = new Block(randomCol, randomRow);
-};
-
-const apple = new Apple();
-apple.move()
-apple.draw();
-
 const intervalId = setInterval(function () {
   ctx.clearRect(0, 0, width, height);
   // drawScore()
-  snake.move();
   snake.draw();
+  snake.move();
   apple.draw();
   drawBorder();
-},100)
+}, 100)
+
+function gameOver() {
+  result.textContent = 0;
+  snake = new Snake();
+}
